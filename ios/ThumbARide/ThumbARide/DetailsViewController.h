1071da2ca02c2ed870ab1dd71ba17f777d2a8754
Alex Chugunov <achugunov@pandora.com>
2015-11-18 16:59:54 -0800

    Added details view controller skeleton

---
+++ ios/ThumbARide/ThumbARide/DetailsViewController.h
@@ -0,0 +1,15 @@
+//
+//  DetailsViewController.h
+//  ThumbARide
+//
+//  Created by Alex Chugunov on 11/18/15.
+//  Copyright © 2015 Pandora. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailsViewController : UIViewController
+
+@property (nonatomic, strong) NSDictionary *user;
+
+@end
---
+++ ios/ThumbARide/ThumbARide/DetailsViewController.m
@@ -0,0 +1,37 @@
+//
+//  DetailsViewController.m
+//  ThumbARide
+//
+//  Created by Alex Chugunov on 11/18/15.
+//  Copyright © 2015 Pandora. All rights reserved.
+//
+
+#import "DetailsViewController.h"
+
+@interface DetailsViewController ()
+
+@end
+
+@implementation DetailsViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
