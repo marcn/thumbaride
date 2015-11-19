957b60026cd7cf9606931d68a73bc8679226b44b
Marc Novakowski <marc@pandora.com>
2015-11-18 16:15:45 -0800

    Adding Facebook auth to server

--- meteor/thumbaride.js
+++ meteor/thumbaride.js
@@ -3,21 +3,50 @@ if (Meteor.isClient) {
   Session.setDefault('counter', 0);
 
   Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
+	counter: function () {
+	  return Session.get('counter');
+	}
   });
 
   Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
+	'click button': function () {
+	  // increment the counter when button is clicked
+	  Session.set('counter', Session.get('counter') + 1);
+	}
   });
 }
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
+  Meteor.methods({
+	"mobileLogin" : function(args) {
+	  var new_user = args[0];
+
+	  var user = db.users.find({ 
+		"services" : {
+		  "facebook" : {
+			"id" : new_user["id"]
+		  }
+		}
+	  });
+
+	  if ( user == null) {
+		var user_id = Users.insert({
+		  "profile" : {
+			"name" : new_user["name"]
+		  }, "services" : {
+			"facebook" : {
+			  "accessToken" : new_user[""],
+			  "email" : new_user["email"],
+			  "first_name" : new_user["first_name"],
+			  "last_name" : new_user["last_name"],
+			  "id" : new_user["id"],
+			  "name" : new_user["name"],
+			}
+		  }
+		}); 
+	  }
+
+	  return {"method" : "login", "data" : {"userId" : user_id}};
+	}
   });
 }
