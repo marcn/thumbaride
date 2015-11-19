75249f45f2e513a567da0c619a37843d22ea1d19
Marc Novakowski <marc@pandora.com>
2015-11-19 12:39:32 -0800

    Updating seed data with working FB ids.

--- meteor/server/seedCollectionData.js
+++ meteor/server/seedCollectionData.js
@@ -13,7 +13,7 @@ Meteor.startup(function () {
 		Drivers.insert({
 			user_id: 2,
 			name: "Chris Martin",
-			fb_id: "10155900444455527",
+			fb_id: "500010526",
 			lat: "37.885309",
 			long: "-122.266338"
 		});
@@ -23,9 +23,17 @@ Meteor.startup(function () {
 		Passengers.insert({
 			user_id: 3,
 			name: "Chris Phillips",
-			fb_id: "1051125341570270",
+			fb_id: "100000184415527",
 			lat: "37.971545",
 			long: "-122.027786"
+		});
+
+		Passengers.insert({
+			user_id: 4,
+			name: "Tom Conrad",
+			fb_id: "500013004",
+			lat: "37.887345",
+			long: "-122.081706"
 		});
 	}
 
