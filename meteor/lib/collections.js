8d680475f4c4558ded25855d0007d7455752d309
Marc Novakowski <marc@pandora.com>
2015-11-19 17:02:58 -0800

    Removing collections that aren't used anymore

--- meteor/lib/collections.js
+++ meteor/lib/collections.js
@@ -1,6 +1,3 @@
 
-Markers = new Mongo.Collection('markers');
-Drivers = new Mongo.Collection('drivers');
-Passengers = new Mongo.Collection('passengers');
 Pandas = new Mongo.Collection('pandas');
 
